60a0cf9b0ab0e7d626419f2c6334b8ef3d07a7d0
Minh Pham <phamduya@sheridancollege.ca>
2025-07-21 00:09:47 -0400

    Init typescript code for front-end

---
+++ front-end/fe-user-management/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header >
+        <h1>Hello</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
